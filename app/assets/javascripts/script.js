58b225e7fc1b0920d9d90d9e525285f789fabf8c
Paolo Cosentino <cosepaol88@gmail.com>
2015-04-30 19:46:14 -0500

    added category images

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,7 +3,13 @@ $( document ).ready(function() {
     	swal("Shhhhhh....", "Are you serious? Try again hurry up, this doesnt count..", "error");
     });
     $(".correct").on("click", function(){
-    	swal("Good job!", "You are correct!", "success");
-    	$("#next").get(0).click();
+    	swal({
+    		title: "Good job!",
+    		text: "You are correct!",
+    		type: "success"
+    	},
+    	function(isConfirm){
+    		$("#next").get(0).click();
+    	});
     });
 });
